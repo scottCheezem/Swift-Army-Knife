3754e95ada31b4726539d7cc0a770502533655de
scottCheezem <scott.cheezem@gmail.com>
2015-09-03 14:31:08 -0400

    Initial Commit

---
+++ webThumb/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  webThumb
+//
+//  Created by Scott Cheezem on 9/3/15.
+//  Copyright (c) 2015 Scott Cheezem. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
